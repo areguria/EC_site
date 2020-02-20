ee355ce0f5dd39c6b3c7baa4f84cf49fb0efee31
WataruHigaki-web <wataruhigaki@gmail.com>
2020-02-20 11:07:48 +0000

    Start user-mypage

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,11 @@
+class CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+  def update
+  end
+  def confirm
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :customers,only:[:show,:update,:edit]
+  get 'customers/confirm'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

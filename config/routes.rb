3209ddd0d4f30304dd25f98b30f98508d65b1b56
WataruHigaki-web <wataruhigaki@gmail.com>
2020-02-22 09:48:41 +0000

    Add show, create product model

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'customers/confirm'
 
   root 'products#top'
+  get 'products/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end

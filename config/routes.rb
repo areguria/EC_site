24fb7f29e81307861a6c37995eefbdb2c4b544aa
yowashi <hinzyakushi@gmail.com>
2020-02-22 05:49:05 +0000

    [devise作成/EC_top修正]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_for :customers
+  devise_for :admins
   root 'products#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'products#top'
 
 end

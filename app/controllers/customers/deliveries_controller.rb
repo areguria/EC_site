a1321e3bc5e94618e005b49353cae9af00661ae3
yowashi <hinzyakushi@gmail.com>
2020-02-27 12:46:28 +0000

    create_product_index

--- app/controllers/customers/deliveries_controller.rb
+++ app/controllers/customers/deliveries_controller.rb
@@ -1,10 +1,25 @@
 class Customers::DeliveriesController < ApplicationController
 	def index
+		@delivery = Delivery.new
 	end
+
 	def edit
 	end
+
+	def create
+		delivery = Delivery.new(delivery.params)
+		delivery.save
+		redirect_to customers_deliveries_path
+	end
+
 	def update
 	end
+
 	def destroy
+	end
+
+	private
+	def delivery.params
+		params.require(:delivery).permit(:address,:zip_code,:name)
 	end
 end

2bce4fb49a9c01374cd17c99623e2f450f6197f6
yowashi <hinzyakushi@gmail.com>
2020-03-01 09:42:51 +0000

    cart-item 重複して入ることの修正

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,26 +1,39 @@
 class Customers::CartItemsController < ApplicationController
 
-  def show
-  	@cart_items = current_customer.cart_items
+  def index
+  	@cart_item = current_customer.cart_items
   end
 
   def create
-    cart_item = CartItem.new(cart_item_params)
-    # cart_items = Product.find_by(id: cart_item_params[:product_id])
-    cart_item.customer_id = current_customer
-  	cart_item.save
-    redirect_to customers_cart_item_path(current_customer)
-
+    cart_items = CartItem.all
+    isExist = false
+    id = 0
+    cart_items.each do |cart_item|
+      if cart_item.product_id == cart_item_params[:product_id].to_i
+        isExist = true
+        id = cart_item.id
+      end
+    end
+    if isExist
+      cart_item = CartItem.find(id)
+      sum = cart_item.count.to_i + cart_item_params[:count].to_i
+      cart_item.update_attributes(count: sum)
+      redirect_to customers_cart_items_path
+    else
+      cart_item = CartItem.new(cart_item_params)
+    cart_item.customer_id = current_customer.id
+    cart_item.save
+    redirect_to customers_cart_items_path
+    end
 	end
 
   def destroy
-  	@cart_item = current_cart_item
-  	@cart_item.destroy
-  	redirect_to customers_cart_items_path(current_customer)
+  	cart_item = CartItem.find(params[:id])
+  	cart_item.destroy
+  	redirect_to customers_cart_items_path
   end
 
   private
-  
   def cart_item_params
   	params.require(:cart_item).permit(:product_id,:count)
   end

df30eca3cf6f944f35ca9f2f6de492bd92ea7fce
yowashi <hinzyakushi@gmail.com>
2020-03-14 06:12:07 +0000

    finish カテゴリー別

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -9,9 +9,17 @@ class Customers::ProductsController < ApplicationController
 	end
 	PER = 8
 	def index
-		@products = Product.page(params[:page]).per(PER)
-		@categories = Category.all
-		@allcount = Product.all.count
+		if params[:category_id]
+			@category = Category.find(params[:category_id])
+			@products = @category.products.all
+			@categories = Category.all
+			@allcount = @category.products.all.count
+			@products = @category.products.page(params[:page]).per(PER)
+		else
+			@products = Product.page(params[:page]).per(PER)
+			@categories = Category.all
+			@allcount = Product.all.count
+		end
 	end
 
 	private
@@ -19,5 +27,4 @@ class Customers::ProductsController < ApplicationController
 	def product_params
 		 params.require(:product).permit(:name,:introduction,:category_id,:product_image,:status,:price)
  	end
-
 end
